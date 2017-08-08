094d9fba0592edf40d2a163c863b26b6b7549760
Eric Meyer <eric@curiousm.com>
2017-08-08 19:57:33 -0400

    Do this
    Signed-off-by: Eric Meyer <eric@curiousm.com>

--- app/controllers/input.js
+++ app/controllers/input.js
@@ -1,10 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  inputText:null,
+
+  init(){
+    console.log('controller init function');
+    this.set('inputText',null);
+  },
 
   actions: {
       calculateValue(){
+        // Both of these need to be selected. if the dropdown is null, execution will fail
         console.log(this.get('psuS').get('supplyVoltage'));
+        console.log(this.get('inputText'));
       }
   }
 
